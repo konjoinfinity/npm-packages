326dc4c5a92132698332e37825c5eeaa0deb4654
Wesley Scholl <wesleyscholl@gmail.com>
2023-03-02 11:12:52 -0500

    Create myApp.js

---
+++ myApp.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let app = express();
+
+console.log('Hello World')
+
+  
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+
+
+
+
+
+module.exports = app
